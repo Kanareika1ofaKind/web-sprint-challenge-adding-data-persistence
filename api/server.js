d1239511dddc74323873649405dca7e6892792cc
Sydney <proteimus@gmail.com>
2025-07-04 15:45:42 -0500

    Migration-n-Router setter

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require("express")
+
+const projectRouter = require("./project/router")
+
+const resourceRouter = require("./resource/router")
+
+const taskRouter = require("./task/router")
+
+const server = express()
+
+server.use(express.json())
+
+server.use("/api/project", projectRouter)
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" })
+})
+
+module.exports = server
+
