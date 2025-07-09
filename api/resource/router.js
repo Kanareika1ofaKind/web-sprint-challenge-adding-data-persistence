89ae52378ee4f7ecf8f14548f581a2a214f14b66
Sydney <proteimus@gmail.com>
2025-07-09 15:06:45 -0500

    editing NEXT function

--- api/resource/router.js
+++ api/resource/router.js
@@ -8,16 +8,16 @@ const router = express.Router();
 const Resource = require('./model.js'); // import the Resource model
 const { checkResourceNameExists } = require('./middleware.js'); // import the middleware to check resource name
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         const resources = await Resource.getAll();
         res.status(200).json(resources);
     } catch (error) {
-        res.status(500).json({ message: 'Error retrieving resources!', error });
+        next({ message: 'Error retrieving resources!', error });
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     const { id } = req.params;
     try {
         const resource = await Resource.getById(id);
@@ -26,18 +26,18 @@ router.get('/:id', async (req, res) => {
         }
         res.status(200).json(resource);
     } catch (error) {
-        res.status(500).json({ message: 'Error retrieving resource!', error });
+        next({ message: 'Error retrieving resource!', error });
     }
 });
 
-router.post('/', checkResourceNameExists, async (req, res) => {
+router.post('/', checkResourceNameExists, async (req, res, next ) => {
     const { resource_name, resource_description } = req.body; // destructure the request body 
     const resourceData = { resource_name, resource_description }; // create a new resource object    
         try {
             const newResource = await Resource.create(resourceData); // create a new resource
             res.status(201).json(newResource); // respond with the created resource
         } catch (error) {
-            res.status(500).json({ message: 'Error creating resource!', error });
+            next({ message: 'Error creating resource!', error });
         }    
 });
 
